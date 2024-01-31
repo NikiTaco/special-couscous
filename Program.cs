05f52948e7fd401550149fb9444217680085c867
McRaser <kanapushka30@gmail.com>
2024-01-31 19:06:06 +0300

    Задача 1 решена

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿void WriteNumbers(int m, int n)
+{
+if (m > n)
+return;
+Console.Write(m + " " );
+WriteNumbers(m+1,n);
+}
+Console.Write("Введите значение M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+WriteNumbers(m,n);
+
+
